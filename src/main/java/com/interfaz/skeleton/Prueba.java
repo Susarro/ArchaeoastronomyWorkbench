fb2d8b4ec6284b39f815b4857f35d2cdd9aaf5ac
MIGUEL_ANGEL <MIGUEL_ANGEL@USUARIO-TOSH.rdn.acxgroup.com>
2017-09-07 20:40:39 +0200

    Maven

---
+++ src/main/java/com/interfaz/skeleton/Prueba.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.interfaz.skeleton;
+
+import com.Global;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author MIGUEL_ANGEL
+ */
+public class Prueba extends Application
+{
+
+    Skeleton esqueleto;
+    VBox raiz;
+    private Button btn;
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        esqueleto = new Skeleton(System.getProperty("user.home") + "\\N2ROLDAN", "prueba", primaryStage, null);
+        raiz = new VBox();
+        Scene scene = new Scene(raiz);
+        scene.getStylesheets().add(Skeleton.class.getResource("/styles/general.css").toExternalForm());
+        esqueleto.setScene(scene);
+        primaryStage.setTitle("Prueba");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(true);
+        raiz.getStyleClass().add("fondo");
+        primaryStage.show();
+        RoundButton rb;
+        HBox h = new HBox(10);
+        //h.getChildren().addAll(new Label("Prueba"), new CheckBox("Pica"), new TitledPane("Hola", new Label("Hola")));
+        GridPane grid = new GridPane();
+        h.getChildren().addAll(new BorderedTitledPane("Hola", grid));
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.add(new Label("Patata"), 0, 0);
+        grid.add(new Label("Castaña"), 0, 1);
+        grid.add(new Label("Melón"), 1, 0);
+        grid.add(new Label("Pimiento"), 1, 1);
+        raiz.getChildren().addAll(new Label("Hola"), h, new ComboBox(FXCollections.observableArrayList("Todos", "A", "B", "C", "D", "E", "F")), btn = new Button("Hola"));
+        btn.setTooltip(new Tooltip("Hola"));
+        Menu m;
+        MenuItem mi;
+        MenuBar menu=new MenuBar();
+        menu.getMenus().add(m = new Menu("Menú"));
+        CheckMenuItem cmi = new CheckMenuItem("Check");
+        m.getItems().add(cmi);
+        mi = new MenuItem("Normal");
+        m.getItems().add(mi);
+        
+        raiz.getChildren().add(menu);
+
+        Global.info.info("Hola");
+
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+}
---
+++ src/main/java/com/interfaz/skeleton/Skeleton.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.interfaz.skeleton;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ * A skeleton for javaFX programs
+ * @author MIGUEL_ANGEL
+ */
+public class Skeleton
+{
+    /**
+     * Main stage
+     */
+    protected Stage mainStage;
+    /**
+     * Main scene
+     */
+    public Scene mainScene;
+    
+    /**
+     * Work directory
+     */
+    public final String workDir;
+    /**
+     * Name
+     */
+    private final String name;
+    
+    
+    /**
+     * 
+     * @return main stage
+     */
+    public Stage getStage()
+    {
+        return mainStage;
+    }
+    
+    /**
+     * 
+     * @param st Stage to set
+     */
+    public void setStage(Stage st)
+    {
+        this.mainStage=st;
+    }
+
+    /**
+     * 
+     * @return Main scene
+     */
+    public Scene getScene()
+    {
+        return mainScene;
+    }
+    
+    /**
+     * 
+     * @param scene Scene to set
+     */
+    public void setScene(Scene scene)
+    {
+        this.mainScene=scene;
+    }
+                
+    
+         
+    /**
+     * 
+     * @param workDir Work directory
+     * @param name Name
+     * @param st Main Stage
+     * @param sc Main Scene
+     */
+    public Skeleton(String workDir, String name, Stage st, Scene sc)
+    {
+        this.mainStage=st;
+        this.mainScene=sc;
+        if(!workDir.endsWith(System.getProperty("file.separator"))) workDir+=System.getProperty("file.separator");
+        this.workDir=workDir;
+        this.name=name;
+        
+        Properties props = new Properties();
+        try
+        {
+            try (InputStream configStream = Skeleton.class.getResourceAsStream("/conf/log.properties"))
+            {
+                props.load(configStream);           
+            }
+            
+            props.setProperty("log4j.appender.DAILYFILE.file", workDir + "log"+System.getProperty("file.separator")+name);
+            LogManager.resetConfiguration();
+            PropertyConfigurator.configure(props);
+        }
+        catch (IOException e)
+        {
+            PropertyConfigurator.configure(Skeleton.class.getResource("/conf/log.properties"));
+        }
+    }        
+       
+    
+}
